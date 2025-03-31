6f1d5806c2eaf7fcd5f2e5a221ea1774b14e4326
LiuShengYuan123 <1574020265@qq.com>
2025-03-31 12:31:35 +0800

    修正工作流

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,15 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  // output: "export", // 启用静态导出模式
-  
-  // // 必需配置：禁用图片优化
-  // images: {
-  //   unoptimized: true
-  // },
-
-  // 可选配置：自定义构建目录（默认就是 out）
-  distDir: "out", // 默认值，无需显式声明
+  distDir: 'out'
 };
 
 export default nextConfig;
